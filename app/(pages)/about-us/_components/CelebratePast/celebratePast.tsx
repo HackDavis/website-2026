3d831f512d2b261dedf87388940ce25498b40b53
phlangiee <174984579+phlangiee@users.noreply.github.com>
2026-01-18 17:40:10 -0800

    from the techathon

--- app/(pages)/about-us/_components/CelebratePast/celebratePast.tsx
+++ app/(pages)/about-us/_components/CelebratePast/celebratePast.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import React from 'react';
 import styles from './celebratePast.module.scss';
 import font_string from '@app/(pages)/_globals/fonts';
 import { useState, useEffect } from 'react';
@@ -11,86 +12,103 @@ enum Type {
     Wide
 }
 
-function Pictures({ path, alt, order }: { path: string, alt: string, order: number }) {
+function Pictures({ path, alt, type, order }: { path: string, alt: string, type: Type, order: number }) {
     const newOrder = 4 - order; //100 is length of the images
 
+    let shape;
+    let frame;
     let picture;
 
-
-    if (newOrder === 1) {
-        picture =  (
-            <div className="rotate-[0deg] absolute inset-0 flex justify-center items-center p-2 pb-[60px] bg-[#DCE3EA]">
-                <div className="w-[200px] h-[200px]">
-                    <Image src={path} alt={alt} width={200} height={200} className="object-fill"/>
-                </div>
-                {newOrder} {order}
+    if (type === Type.Square) {
+        picture = (
+            <div className="relative w-[250px] h-[250px] bg-red-500 overflow-hidden">
+                <Image src={path} alt={alt} fill className="object-cover" />
             </div>
         );
-    } else if (newOrder === 2) {
-        picture =  (
-            <div className="rotate-[116.01deg] absolute inset-0 flex justify-center items-center p-2 pb-[60px] bg-[#BDC7D0]">
-                <div className="w-[200px] h-[200px]">
-                    <Image src={path} alt={alt} width={200} height={200} className="object-cover"/>
-                </div>
-                
-                {newOrder} {order}
+    } else if (type === Type.Long) {
+        picture = (
+            <div className="relative w-[200px] h-[300px] bg-red-500 overflow-hidden">
+                <Image src={path} alt={alt} fill className="object-cover" />
             </div>
         );
-    } else if (newOrder === 3) {
-        picture =  (
-            <div className="rotate-[92.8deg] absolute inset-0 flex justify-center items-center p-2 pb-[60px] bg-[#BDC7D0]">
-                <div className="w-[200px] h-[200px]">
-                    <Image src={path} alt={alt} width={200} height={200} className="object-cover"/>
-                </div>
-                
-                {newOrder} {order}
-            </div>
-        );
-    } else if (newOrder === 4) {
-        picture =  (
-            <div className="rotate-[76.15deg] absolute inset-0 flex justify-center items-center p-2 pb-[60px] bg-[#BDC7D0]">
-                <div className="w-[200px] h-[200px]">
-                    <Image src={path} alt={alt} width={200} height={200} className="object-cover"/>
-                </div>
-                {newOrder} {order}
+    } else if (type === Type.Wide) {
+        picture = (
+            <div className="relative w-[300px] h-[200px] bg-red-500 overflow-hidden">
+                <Image src={path} alt={alt} fill className="object-cover" />
             </div>
         );
     } else {
-        picture =  (
-            <div className="rotate-[0deg] absolute inset-0 flex justify-center items-center p-2 pb-[60px] bg-[#BDC7D0]">
-                <div className="w-[200px] h-[200px]">
-                    <Image src={path} alt={alt} width={200} height={200} className="object-cover"/>
-                </div>
-                {newOrder} {order}
+        picture = (
+            <p>ERROR: Unknown type of Image</p>
+        );
+    }
+
+    if (newOrder === 1) {
+        frame = (
+            <div className="rotate-[0deg] absolute inset-0 flex justify-center items-center p-2 pb-[40px] bg-[#DCE3EA]">
+                {picture}
+            </div>
+        );
+    } else if (newOrder === 2) {
+        frame = (
+            <div className="rotate-[345deg] translate-x-[-100px] translate-y-[-40px] absolute inset-0 flex justify-center items-center p-2 pb-[40px] bg-[#BDC7D0]">
+                {picture}
+            </div>
+        );
+    } else if (newOrder === 3) {
+        frame = (
+            <div className="rotate-[-5deg] translate-y-[-100px] absolute inset-0 flex justify-center items-center p-2 pb-[40px] bg-[#BDC7D0]">
+                {picture}
+            </div>
+        );
+    } else if (newOrder === 4) {
+        frame = (
+            <div className="rotate-[20deg] translate-x-[100px] translate-y-[-30px] absolute inset-0 flex justify-center items-center p-2 pb-[40px] bg-[#BDC7D0]">
+                {picture}
+            </div>
+        );
+    } else {
+        frame = (
+            <div className="rotate-[0deg] absolute inset-0 flex justify-center items-center p-2 pb-[40px] bg-[#BDC7D0]">
+                {picture}
             </div>
         );
     }
 
-
-
-    // 0 116.01 92.8 76.15
-    let specificStyle = {
-        zIndex: newOrder,
-        transform: `rotate(0deg)`
-    };
-    if (newOrder === 0) {
-        //animation = 0;
-    } else if (newOrder === 1) {
-        //animation = 10;
-    } else if (newOrder === 2) {
-        //animation = 20;
-    } else if (newOrder === 3) {
-        //animation = 30;
+    if (type === Type.Square) {
+        shape = (
+            <div className="absolute w-[265px] h-[300px] animate-enter1">
+                {frame}
+            </div>
+        );
+    } else if (type === Type.Long) {
+        shape = (
+            <div className="absolute w-[215px] h-[350px] animate-enter1">
+                {frame}
+            </div>
+        );
+    } else if (type === Type.Wide) {
+        shape = (
+            <div className="absolute w-[315px] h-[250px] animate-enter1">
+                {frame}
+            </div>
+        );
+    } else {
+        shape = (
+            <div className="absolute w-[315px] h-[350px] animate-enter1">
+                {frame}
+            </div>
+        );
     }
-    return picture;
+    return shape;
 }
 
 export default function CelebratePast() {
     const [images, setImages] = useState([
         { path: "/placeholder.jpg", alt: "among us", type: Type.Square, order: 1 },
-        { path: "/anotherplaceholder.png", alt: "among us!!!", type: Type.Square, order: 2 },
-        { path: "/clouds.jpg", alt: "among us!!!", type: Type.Square, order: 3 },
-        { path: "/readclouds.jpg", alt: "among us!!!", type: Type.Square, order: 4 },
+        { path: "/anotherplaceholder.png", alt: "among us!!!", type: Type.Long, order: 2 },
+        { path: "/clouds.jpg", alt: "among us!!!", type: Type.Wide, order: 3 },
+        { path: "/realclouds.jpg", alt: "among us!!!", type: Type.Wide, order: 4 },
     ]);
 
     useEffect(() => {
@@ -101,7 +119,7 @@ export default function CelebratePast() {
                     order: (img.order + 1) % prevImages.length,
                 }));
             });
-        }, 2000); //10sec
+        }, 10000); //10sec
 
         return () => clearInterval(interval); //E5EEF1 var(--text-extra-dark)
     }, []);
@@ -119,14 +137,15 @@ export default function CelebratePast() {
                 </div>
             </div>
             {/** uhhh the pictures F1F3F8*/}
-            <div className="relative w-[300px] h-[300px]"> {/* Container must be relative */}
+            <div className="relative w-[300px] h-[300px] flex items-center justify-center"> 
                 {sortedImages.map((image) => (
-                        <Pictures
-                            key={image.path}
-                            path={image.path}
-                            alt={image.alt}
-                            order={image.order}
-                        />
+                    <Pictures
+                        key={image.path}
+                        path={image.path}
+                        alt={image.alt}
+                        type={image.type}
+                        order={image.order}
+                    />
                 ))}
             </div>
             <button className="pl-[17px] pr-[17px] pt-[5px] pb-[5px] m-4 mb-[50px] rounded-full bg-[#123041] text-[#F9FBFC]">
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,22 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      keyframes: {
+        enter1: {
+          '0%': { transform: 'translateY(0px)' },
+          '10%': { transform: 'translateY(100px)' },
+          '90%': { transform: 'translateY(500px)' },
+          '100%': { transform: 'translateY(600px)' },
+        },
+        fadeOut: {
+          '0%': { opacity: '1', transform: 'scale(1)' },
+          '100%': { opacity: '0', transform: 'scale(1.05)' },
+        },
+      },
+      animation: {
+        enter1: 'enter1 10000ms ease-out infinite',
+        fadeOut: 'fadeOut 300ms ease-in forwards',
+      },
     },
   },
   plugins: [],
