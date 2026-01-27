ee32f2844493aade6f6d9b48d3dd1f8658feed90
jackzheng-cs <134164876+jackzheng-cs@users.noreply.github.com>
2026-01-27 09:37:47 -0800

    #2 what we're all about (#10)
    * Add What We're All About section
    
    * Change folders and rename AllAbout section
    
    * Update format
    
    * Add first mobile changes
    
    * Add final mobile changes
    
    * Updated file structure, implemented grid instead of flexbox
    
    * Stagger
    
    * Animation duration
    
    * Fixed padding and component stacking
    
    ---------
    
    Co-authored-by: Haylie Tan <haytan@ucdavis.edu>

--- app/(pages)/about-us/page.tsx
+++ app/(pages)/about-us/page.tsx
@@ -1,11 +1,13 @@
 import Hero from '@app/(pages)/about-us/_components/Hero/Hero';
 import Header from '@components/Header/Header';
+import AllAbout from './_components/AllAbout/AllAbout';
 
 export default function AboutUs() {
   return (
     <div className="relative">
       <Header />
       <Hero />
+      <AllAbout />
     </div>
   );
 }
