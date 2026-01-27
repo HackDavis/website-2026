52cfa49357e3f49d8a80ab0fa04afd26a2222a06
Haylie Tan <haytan@ucdavis.edu>
2026-01-27 09:43:15 -0800

    Update page.tsx

--- app/(pages)/about-us/page.tsx
+++ app/(pages)/about-us/page.tsx
@@ -2,12 +2,14 @@ import Hero from '@app/(pages)/about-us/_components/Hero/Hero';
 import Header from '@components/Header/Header';
 import Directors from './_components/Directors/directors';
 import OurTeam from './_components/OurTeam/OurTeam';
+import AllAbout from './_components/AllAbout/AllAbout';
 
 export default function AboutUs() {
   return (
     <div className="relative">
       <Header />
       <Hero />
+      <AllAbout />
       <Directors />
       <OurTeam />
     </div>
