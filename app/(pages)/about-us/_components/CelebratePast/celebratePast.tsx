2a58565ec0a91d7e3446802e7017ede919885414
phlangiee <174984579+phlangiee@users.noreply.github.com>
2026-02-02 17:27:11 -0800

    CelebratePast (#23)
    * test
    
    * images for testing
    
    * pictures function
    
    * switcting the pictures
    
    * words
    
    * pictures and button
    
    * imports
    
    * scss
    
    * packages?
    
    * Name changes
    
    * added another temp image
    
    * another image?
    
    * nope sorry this is the image
    
    * from the techathon
    
    * what happened to the semicolon
    
    * animation....
    
    * going back to lowercase for now
    
    * fixed order and animation
    
    * just in case i need my previous code (ill delete later)
    
    * frog image :3
    
    * YIPEEE POLISHED THE ANIMATION AND ENTIRE THING!!!
    
    * better animation
    
    * shorter pictures func
    
    * no picture for images over order 5
    
    * um i forgot but this is also apart of shotrer pictures
    
    * fixed beginning glitch when swapping orders?
    
    * changed key for pictures
    
    * no italiac for button (i dont like it)
    
    * more rounded
    
    * FIXED THE GLITCH IN THE LOOPING!!!
    
    * only switches when animation completes
    
    * faster animation for picture 1 going to the back
    
    * cleaning up code
    
    * different views :3
    
    * changed animation to percentages + deleted bunch of comments
    
    * a space (for lint)
    
    * linting + changed to percentages
    
    * capitalized celetebratepast
    
    * Updated ESLint errors, fixed layout.tsx
    
    * Update page.tsx
    
    * Update tailwind config
    
    * Fix dependencies
    
    * Build errors
    
    * Working w. gltich
    
    * Added placeholder images
    
    * Linting
    
    * linting + errors
    
    * Update CarouselWindow.tsx
    
    * Updated images
    
    ---------
    
    Co-authored-by: Haylie Tan <haytan@ucdavis.edu>

---
+++ app/(pages)/about-us/_components/CelebratePast/celebratePast.tsx
@@ -0,0 +1,111 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
+
+type Img = { path: string; alt: string };
+
+function Pictures({
+  path,
+  alt,
+  order,
+}: {
+  path: string;
+  alt: string;
+  order: number;
+}) {
+  if (order > 5) return null;
+
+  const animClass =
+    order === 1
+      ? 'animate-picture1 bg-[#BDC7D0]'
+      : order === 2
+      ? 'animate-picture2 bg-[#BDC7D0]'
+      : order === 3
+      ? 'animate-picture3 bg-[#BDC7D0]'
+      : order === 4
+      ? 'animate-picture4 bg-[#BDC7D0]'
+      : order === 5
+      ? 'animate-picture5 bg-[#BDC7D0]'
+      : '';
+
+  return (
+    <div
+      className={`absolute left-1/2 top-1/2 will-change-transform flex justify-center items-center p-[0.8cqw] pb-[5cqw] ${animClass}`}
+    >
+      <div className="w-[18cqw] h-[18cqw] relative overflow-hidden">
+        <Image src={path} alt={alt} fill className="object-cover" />
+      </div>
+    </div>
+  );
+}
+
+export default function CelebratePast() {
+  const [images, setImages] = useState<Img[]>([
+    { path: '/Images/celebrateus/One.jpeg', alt: 'i like among us' },
+    { path: '/Images/celebrateus/Two.jpg', alt: 'its pretty cool' },
+    { path: '/Images/celebrateus/Three.jpg', alt: 'cheese is also cool' },
+    { path: '/Images/celebrateus/Four.jpg', alt: 'amongsus' },
+    { path: '/Images/celebrateus/Five.jpg', alt: 'among us!!!' },
+  ]);
+
+  useEffect(() => {
+    // match this interval to your CSS animation duration
+    const DURATION_MS = 3000;
+
+    const id = window.setInterval(() => {
+      setImages((prev) => {
+        const [first, ...rest] = prev;
+        return [...rest, first];
+      });
+    }, DURATION_MS);
+
+    return () => window.clearInterval(id);
+  }, []);
+
+  return (
+    <div className="m-[10%] container-type-inline-size py-[5%] rounded-[8cqw] shadow-[2cqw_2cqw_0_0_#A6BFC7] outline outline-[0.3cqw] outline-[#A6BFC7] md:outline-[0cqw] flex flex-col items-center bg-[#E5EEF1] place-content-center">
+      {/* text */}
+      <div className="flex flex-col items-center">
+        <div className="py-[0.5cqw] text-center">
+          <p className="text-[5cqw] md:text-[3cqw] leading-tight text-[#123041] font-bold font-montserrat">
+            Come celebrate
+            <br className="md:hidden" /> our past with us!
+          </p>
+        </div>
+        {/* paragraph */}
+        <div className="py-[0.5cqw] text-center ">
+          <p className="text-[2.4cqw] md:text-[1.2cqw] text-[#123041] font-jakarta">
+            We truly could not have done any of this
+            <br className="md:hidden" /> without you. Your support means the
+            world!
+          </p>
+        </div>
+      </div>
+
+      <div className="h-[40cqw] relative flex items-center justify-center">
+        {images.map((image, index) => (
+          <Pictures
+            // key={`${image.path}-${index}`}
+            key={image.path}
+            path={image.path}
+            alt={image.alt}
+            order={index + 1}
+          />
+        ))}
+      </div>
+
+      <button className="px-[3cqw] py-[1cqw] md:px-[1.5cqw] md:py-[.5cqw] rounded-full bg-[#123041] text-[#F9FBFC]">
+        <a
+          href="https://hackdavis-2025.devpost.com/project-gallery"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <p className="text-[3cqw] md:text-[1.3cqw] font-medium font-jakarta tracking-wide">
+            VIEW 2025 WINNERS
+          </p>
+        </a>
+      </button>
+    </div>
+  );
+}
