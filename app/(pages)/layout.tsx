05aac798d091199297f030a07fdece8da8e4fdb5
reehals <sunny.reehal@gmail.com>
2026-02-05 02:13:20 -0800

    Wrap footer for multi styling and including metadata

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,28 +1,23 @@
-'use client';
-import { usePathname } from 'next/navigation';
 import type { Metadata } from 'next';
 import '@globals/styles/globals.scss';
 import metadataJSON from '@app/(pages)/_globals/metadata.json';
 import fonts from './_globals/fonts';
 import Header from './_components/Header/Header';
-import Footer from '@app/(pages)/_components/Footer/Footer';
+import FooterWrapper from './_components/FooterWrapper';
 
-// export const metadata: Metadata = metadataJSON;
+export const metadata: Metadata = metadataJSON;
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const pathname = usePathname();
-  const isIndexPage = pathname === '/';
-
   return (
     <html lang="en">
       <body className={`${fonts} antialiased`}>
         <Header />
         {children}
-        <Footer variant={isIndexPage ? 'index' : 'about-us'} />
+        <FooterWrapper />
       </body>
     </html>
   );
