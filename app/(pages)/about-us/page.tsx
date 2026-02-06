369edad6303d9d60f199c88c1abd520925762f67
phlangiee <174984579+phlangiee@users.noreply.github.com>
2026-02-05 16:37:32 -0800

    got rid of the donors component from about us

--- app/(pages)/about-us/page.tsx
+++ app/(pages)/about-us/page.tsx
@@ -1,11 +1,9 @@
 import Hero from '@app/(pages)/about-us/_components/Hero/Hero';
 import Header from '@components/Header/Header';
-import Donors from '../registration/Donors/Donors';
 
 export default function AboutUs() {
   return (
     <div className="relative">
-      <Donors />
     </div>
   );
 }
