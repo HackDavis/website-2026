7a7a2fdfa2835f3c075c60256e71ea14acdc6eaf
phlangiee <174984579+phlangiee@users.noreply.github.com>
2026-02-05 16:11:30 -0800

    Sponsors + donor scroll (registration) Fixes #21
    also i removed the header in about us

--- app/(pages)/about-us/page.tsx
+++ app/(pages)/about-us/page.tsx
@@ -1,11 +1,11 @@
 import Hero from '@app/(pages)/about-us/_components/Hero/Hero';
 import Header from '@components/Header/Header';
+import Sponsers from '../registration/Sponsers/Sponsers';
+import Donors from '../registration/Donors/Donors';
 
 export default function AboutUs() {
   return (
     <div className="relative">
-      <Header />
-      <Hero />
     </div>
   );
 }
